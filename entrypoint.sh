32c0d697dc8382d0bd79b8276bdb4403169ffac5
Michał Papierski <michal@papierski.net>
2019-01-31 12:12:48 +0100

    Set up traps to cleanup Java mess.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
+set -e
+
 export PATH="/opt/libreoffice/program/:$PATH"
-exec soffice $@
+
+function cleanup {
+  if [ -e /tmp/hsperfdata_libreoffice ]; then
+    rm -rf /tmp/hsperfdata_libreoffice
+  fi
+}
+
+trap cleanup EXIT
+
+soffice $@
